bb8e4963d399839370eb08f165868a1d1b8d7b3f
KhrMichael <misha8misha0@gmail.com>
2021-11-10 00:00:39 +0300

    initial commit

---
+++ Task3/NumberSytemConverter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace Task3
+{
+    public class NumberSytemConverter
+    {
+        /// <summary>
+        /// Converts a decimal number into a new number system.
+        /// </summary>
+        /// <param name="decimalNumber">Number in decimal number system</param>
+        /// <param name="numberSysBase">Base of the number system to convert(2 - 20)</param>
+        public string Convert(int decimalNumber, int numberSysBase)
+        {
+            if (numberSysBase > 20 || numberSysBase < 2)
+            {
+                return null;
+            }
+
+            StringBuilder convertedNumber = new StringBuilder();
+            int remainder;
+            while (decimalNumber >= numberSysBase)
+            {
+                remainder = decimalNumber % numberSysBase;
+                decimalNumber /= numberSysBase;
+                convertedNumber.Append(ConvertNumberToDigit(remainder));
+            }
+            convertedNumber.Append(ConvertNumberToDigit(decimalNumber));
+
+            return new string(convertedNumber.ToString().Reverse().ToArray());
+        }
+
+        private char ConvertNumberToDigit(int number)
+        {
+            char digit;
+            if (number > 9)
+            {
+                digit = (char)(65 + number - 10);
+            }
+            else
+            {
+                Char.TryParse(number.ToString(), out digit);
+            }
+            return digit;
+        }
+    }
+}
---
+++ Task3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                NumberSytemConverter numberSytemConverter = new();
+                int.TryParse(args[0], out int decimalNumber);
+                int.TryParse(args[1], out int numberSysBase);
+                Console.WriteLine(numberSytemConverter.Convert(decimalNumber, numberSysBase));
+            }
+        }
+    }
+}
