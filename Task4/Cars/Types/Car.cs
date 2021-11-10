402ed72f9b9c46776bd1829798726bce7c7b21b4
KhrMichael <misha8misha0@gmail.com>
2021-11-10 14:56:12 +0300

    vehicle types added

---
+++ Task4/Cars/Types/Car.cs
@@ -0,0 +1,11 @@
+﻿using Task4.Cars.Parts;
+
+namespace Task4.Cars.Types
+{
+    public class Car : Vehicle
+    {
+        public override Engine Engine { get; protected set; }
+        public override Chassis Chassis { get; protected set; }
+        public override Transmission Transmission { get; protected set; }
+    }
+}
---
+++ Task4/Cars/Types/Vehicle.cs
@@ -0,0 +1,11 @@
+﻿using Task4.Cars.Parts;
+
+namespace Task4.Cars.Types
+{
+    public abstract class Vehicle
+    {
+        public abstract Engine Engine { get; protected set; }
+        public abstract Chassis Chassis { get; protected set; }
+        public abstract Transmission Transmission { get; protected set; }
+    }
+}
