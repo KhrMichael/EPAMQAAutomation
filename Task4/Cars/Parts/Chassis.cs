1c8490f76855876230391935d73af7fb9114e9ab
KhrMichael <misha8misha0@gmail.com>
2021-11-10 14:19:37 +0300

    Chassis added

---
+++ Task4/Cars/Parts/Chassis.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4.Cars.Parts
+{
+    public class Chassis
+    {
+        public int WheelNumber { get; set; }
+        public string Number { get; set; }
+        public double PermissbleLoad { get; set; }
+
+
+    }
+}
