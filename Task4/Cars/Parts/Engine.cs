2b97d91e331672e84f6b0b3d1f1b985f5eab69b9
KhrMichael <misha8misha0@gmail.com>
2021-11-10 14:07:37 +0300

    initial commit engine added

---
+++ Task4/Cars/Parts/Engine.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4.Cars.Parts
+{
+    public class Engine : CarPart
+    {
+        public double Capasity { get; set; }
+        public double Displacement { get; set; }
+        public string Type { get; set; }
+        public string SerialNumber { get; set; }
+    }
+}
