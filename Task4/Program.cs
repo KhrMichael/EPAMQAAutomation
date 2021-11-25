acd486d5c286baa9d5b9a777022f51e1af148a66
KhrMichael <misha8misha0@gmail.com>
2021-11-25 15:59:26 +0300

    removed unnecessary code in Main method

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -7,8 +7,6 @@ namespace Task4
     {
         static void Main(string[] args)
         {
-            Engine engine = new Engine(34, 5, "TYf34", "3754945df854c");
-            Console.WriteLine(engine.GetInfo());
         }
     }
 }
