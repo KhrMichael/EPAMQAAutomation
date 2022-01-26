d56723e897eaf7289dc75d490218c395874a7da3
KhrMichael <misha8misha0@gmail.com>
2022-01-26 19:35:00 +0300

    added VIN custom xml serialization

--- VehicleFleet/Vehicles/Vehicles/VIN.cs
+++ VehicleFleet/Vehicles/Vehicles/VIN.cs
@@ -1,15 +1,32 @@
-﻿namespace VehicleFleet.Vehicles.Vehicles
+﻿using System.Xml;
+using System.Xml.Schema;
+using System.Xml.Serialization;
+
+namespace VehicleFleet.Vehicles.Vehicles
 {
     /// <summary>
     /// Vehicle Identification Number
     /// </summary>
-    public class VIN
+    public class VIN : IXmlSerializable
     {
         public string Number { get; private set; }
 
         public VIN()
         {
             Number = new VINGenerator().GenerateVIN();
+        }
+
+        public XmlSchema GetSchema()
+        {
+            return null;
+        }
+        public void WriteXml(XmlWriter writer)
+        {
+            writer.WriteElementString("Number", Number);
+        }
+        public void ReadXml(XmlReader reader)
+        {
+            Number = reader.ReadElementContentAsString("Number", "");
         }
     }
 }
