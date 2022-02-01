188edfd41974bd6037d7f46311ae2a0b479e6b0a
KhrMichael <misha8misha0@gmail.com>
2022-02-02 01:34:21 +0300

    added: Vehicle

---
+++ ObjectOrientedDesignPrinciplesTask/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ObjectOrientedDesignPrinciplesTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ ObjectOrientedDesignPrinciplesTask/Vehicle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedDesignPrinciplesTask
+{
+    public class Vehicle
+    {
+        public string Type { get; set; }
+        public string Model { get; set; }
+        public uint Quantity { get; set; }
+        public double Price { get; set; }
+    }
+}
