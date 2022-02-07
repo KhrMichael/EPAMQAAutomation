91cecb2b373f1fa7298eaf5945c7810c9107b45c
KhrMichael <misha8misha0@gmail.com>
2022-02-07 20:32:27 +0300

    renamed UnitTest1 to ListExtensionsTest and added VehicleTest

--- ExceptionTaskTests/ListExtensionsTest.cs
+++ ExceptionTaskTests/ListExtensionsTest.cs
@@ -11,22 +11,10 @@ using VehicleFleet.Vehicles.Vehicles;
 namespace ExceptionTaskTests
 {
     [TestClass]
-    public class UnitTest1
+    public class ListExtensionsTest
     {
         private const string vehiclesListPath = "VehiclesList.xml";
 
-        private static IEnumerable<object[]> ActionsWithIncorrectVehiclesInitialization
-        {
-            get
-            {
-                return new[]
-                {
-                    new object[]{ new Action(() => new Truck(new Engine(220, 4.3, "electrical", "34347910DFS"), new Chassis(6, "3434", 12000), new Transmission("auto", 6, "Transmission Inc."))) },
-                    new object[]{ new Action(() => new Scooter(new Engine(220, 4.3, "electrical", "34347910DFS"), new Chassis(4, "344548DKU", 200.25), new Transmission("auto", 6, "Transmission Inc.")))},
-                    new object[]{ new Action(() => new Bus(new Engine(12, 4.3, "electrical", "3253880LPK"), new Chassis(4, "344548DKU", 200.25), new Transmission("auto", 6, "Transmission Inc."))) },
-                };
-            }
-        }
         private static IEnumerable<object[]> IncorrectParametersForAddVehicle
         {
             get
@@ -90,12 +78,6 @@ namespace ExceptionTaskTests
             }
         }
 
-        [TestMethod]
-        [DynamicData("ActionsWithIncorrectVehiclesInitialization")]
-        public void VehicleThrowInitializationExceptionForIncorectInputValues(Action testVehicleInitialization)
-        {
-            Assert.ThrowsException<InitializationException>(testVehicleInitialization);
-        }
 
         [TestMethod]
         [DynamicData("IncorrectParametersForAddVehicle")]
---
+++ ExceptionTaskTests/VehicleTest.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using VehicleFleet.Vehicles.Exceptions;
+using VehicleFleet.Vehicles.Parts;
+using VehicleFleet.Vehicles.Vehicles;
+
+namespace ExceptionTaskTests
+{
+    [TestClass]
+    public class VehicleTest
+    {
+        private static IEnumerable<object[]> ActionsWithIncorrectVehiclesInitialization
+        {
+            get
+            {
+                return new[]
+                {
+                    new object[]{ new Action(() => new Truck(new Engine(220, 4.3, "electrical", "34347910DFS"), new Chassis(6, "3434", 12000), new Transmission("auto", 6, "Transmission Inc."))) },
+                    new object[]{ new Action(() => new Scooter(new Engine(220, 4.3, "electrical", "34347910DFS"), new Chassis(4, "344548DKU", 200.25), new Transmission("auto", 6, "Transmission Inc.")))},
+                    new object[]{ new Action(() => new Bus(new Engine(12, 4.3, "electrical", "3253880LPK"), new Chassis(4, "344548DKU", 200.25), new Transmission("auto", 6, "Transmission Inc."))) },
+                };
+            }
+        }
+
+        [TestMethod]
+        [DynamicData("ActionsWithIncorrectVehiclesInitialization")]
+        public void VehicleThrowInitializationExceptionForIncorectInputValues(Action testVehicleInitialization)
+        {
+            Assert.ThrowsException<InitializationException>(testVehicleInitialization);
+        }
+
+    }
+}
