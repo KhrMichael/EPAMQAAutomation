6fe176a6d074f07e02bc3fd52699fb65887110ea
KhrMichael <misha8misha0@gmail.com>
2022-03-26 17:05:58 +0300

    add ShouldOpenBbcSearchPage test method

--- LocatorsTaskTest/BbcPageTest.cs
+++ LocatorsTaskTest/BbcPageTest.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
@@ -8,11 +9,12 @@ namespace LocatorsTaskTest;
 public class LocatorsTest
 {
     private WebDriver driver;
-    
+
     [TestInitialize]
     public void Initialize()
     {
         driver = new ChromeDriver();
+        driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
     }
 
     [TestMethod]
@@ -21,12 +23,24 @@ public class LocatorsTest
         driver.Url = "https://www.bbc.com/sport";
         var newsButtonXPath = By.XPath("//*[@id=\"orb-nav-links\"]/ul/li[2]");
         var newsButton = driver.FindElement(newsButtonXPath);
-        newsButton.Click();
         
+        newsButton.Click();
+
         Assert.AreEqual("Home - BBC News", driver.Title);
     }
 
-    [TestCleanup]
+    [TestMethod]
+    public void ShouldOpenBbcSearchPage()
+    {
+        driver.Url = "https://www.bbc.com";
+        var searchButton = driver.FindElement(By.Id("orbit-search-button"));
+
+        searchButton.Click();
+        
+        Assert.AreEqual("BBC - Search", driver.Title);
+    }
+
+[TestCleanup]
     public void EndTest()
     {
         driver.Quit();
