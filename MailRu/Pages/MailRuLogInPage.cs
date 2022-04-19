b28ff440777936d8a96cde3ea97b225c8b0f310d
KhrMichael <misha8misha0@gmail.com>
2022-04-19 23:54:26 +0300

    redesign send account name method

--- MailRu/Pages/MailRuLogInPage.cs
+++ MailRu/Pages/MailRuLogInPage.cs
@@ -9,18 +9,13 @@ public class MailRuLogInPage
 {
    private WebDriver Driver { get; }
    private double LoadPageTime => 10;
+   private double SwitchTime => 5;
    private string UniqueElementXPath => "//*[@id='login-content']";
    private string LoginFrameXPath => "/html/body/div[3]/div/iframe";
-   private string InputAccountNameCssSelector =>
-      "#root > div > div > div > div.wrapper-0-2-5 > div > div > form > div:nth-child(2) > div:nth-child(2) >" +
-      " div.login-row.username > div > div > div > div > div > div.base-0-2-58.first-0-2-64 > div > input";
-   private string InputAccountNameXPath =>
-      "//input[@name='username']";
+   private string NameInputXPath => "//input[@name='username']";
    private string SubmitAccountNameButtonCssSelector =>
       "#root > div > div > div > div.wrapper-0-2-5 > div > div > form > div:nth-child(2) > div:nth-child(2) >" +
       " div:nth-child(3) > div > div > div.submit-button-wrap > button";
-   private string InputPasswordXPath =>
-      "//*[@id='root']/div/div/div/div[2]/div/div/form/div[2]/div/div[2]/div/div/div/div/div/input";
    private string InputPasswordName => "password";
    private string SubmitPasswordButtonCssSelector =>
       "#root > div > div > div > div.wrapper-0-2-5 > div > div > form > " +
@@ -57,7 +52,6 @@ public class MailRuLogInPage
 
    public MailRuLogInPage(WebDriver driver, string accountName, string passowrd) : this(driver)
    {
-      Driver = driver;
       AccountName = accountName;
       Passowrd = passowrd;
    }
@@ -70,9 +64,9 @@ public class MailRuLogInPage
 
    public MailRuLogInPage SendAccountName()
    {
-      Driver.SwitchTo().ActiveElement();
-      var input = Driver.FindElement(By.CssSelector(InputAccountNameCssSelector));
-      input.SendKeys(AccountName);
+      var webDriverWait = new WebDriverWait(Driver, TimeSpan.FromSeconds(SwitchTime));
+      var accountName = webDriverWait.Until(driver => driver.FindElement(By.XPath(NameInputXPath)));
+      accountName.SendKeys(AccountName);
 
       return this;
    }
