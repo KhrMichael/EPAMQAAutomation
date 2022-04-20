4132a455b711379b046274e73001b1bf7c2c38aa
KhMichael <misha8misha0@gmail.com>
2022-04-20 15:04:57 +0300

    release

--- LocatorsSandbox/Program.cs
+++ LocatorsSandbox/Program.cs
@@ -6,16 +6,8 @@ chromeDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
 chromeDriver.Url = "https://www.bbc.com/";
 
 //click on the first news on the main page
-var newsButton =
-    chromeDriver.FindElement(By.XPath(
-        "//*[@id='orb-header']//*[@class='orb-nav-newsdotcom']"));
+var newsButton = chromeDriver.FindElement(By.XPath("//*[@id='orb-header']//*[@class='orb-nav-newsdotcom']"));
 newsButton.Click();
-
-//click on the africa news button
-var africaNews =
-    chromeDriver.FindElement(By.XPath(
-        "//*[@class='gs-o-list-ui--top-no-border nw-c-nav__wide-sections']//*[@href='/news/world']"));
-africaNews.Click();
 
 //click on the search button
 var searchButton = chromeDriver.FindElement(By.XPath("//*[@id='orbit-search-button']"));
@@ -28,9 +20,7 @@ var searchInputButton = chromeDriver.FindElement(By.XPath("//*[@id='search-input
 searchInputButton.Click();
 
 //click on the first belarus news
-var belarusFirstNews =
-    chromeDriver.FindElement(By.XPath(
-        "//*[@class='ssrcss-1020bd1-Stack e1y4nx260']/li[1]"));
+var belarusFirstNews = chromeDriver.FindElement(By.XPath("//*[@class='ssrcss-1020bd1-Stack e1y4nx260']/li[1]"));
 belarusFirstNews.Click();
 
 chromeDriver.Close();
